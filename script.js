a4c1c9d236db25ec54eb7166f7c231f9fbb66147
marutovsky <33104282+Marutovsky@users.noreply.github.com>
2024-01-03 13:04:53 +0100

    Create operate() function
    The function takes 3 parameters: firstNumber, operator and
    secondNumber. Next, depending on operator, a proper function executes
    and the operator function returns result of that execution

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(firstNumber, operator, secondNumber) {
+  let result;
+  switch (operator) {
+    case '+':
+      result = addNumbers(firstNumber, secondNumber);
+      break;
+    case '-':
+      result = subtractNumbers(firstNumber, secondNumber);
+      break;
+    case '*':
+      result = multiplyNumbers(firstNumber, secondNumber);
+      break;
+    case '/':
+      result = divideNumbers(firstNumber, secondNumber);
+      break;
+  }
+  return result;
+}
+
 function addNumbers(a, b) {
   return a + b;
 }
